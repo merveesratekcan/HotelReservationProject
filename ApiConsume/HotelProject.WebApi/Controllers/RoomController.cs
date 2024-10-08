f39091f96b14567259dcf854b2ae33c33df658e9
Merve Esra Tekcan <esramerve.tekcan@gmail.com>
2024-10-09 00:08:20 +0300

    Add project files.

---
+++ ApiConsume/HotelProject.WebApi/Controllers/RoomController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult RoomList()
+        {
+            return Ok("Room List");
+        }
+
+        [HttpPost]
+        public IActionResult AddRoom()
+        {
+            return Ok("Room Added");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteRoom()
+        {
+            return Ok("Room Deleted");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateRoom()
+        {
+            return Ok("Room Updated");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetRoom()
+        {
+            return Ok("Room by Id");
+        }
+    }
+}
